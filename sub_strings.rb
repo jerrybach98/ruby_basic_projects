0bfaa541728125c1494fcae083a09cd262ab8599
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-08-21 01:59:55 -0700

    return hash for instances of matching elements and amount found

--- sub_strings.rb
+++ sub_strings.rb
@@ -1,25 +1,23 @@
-#take word as argument and array of substrings (dictionary) as second argument
-#return hash listing each substring (case insensitive), found in original string and how many times it was found
-#Tip recall how to turn string into array and vice versa
-
-#Psuedo
-#convert each word to lowercase and array
-#match each word to dictionary with reduce(hash.new)
-#Return hashes of matches with hash to instances 
-
-word = "Below sit"
+string = "Howdy partner, sit down! How's it going?"
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 
-def substrings(word, dictionary)
-	p word
-	p dictionary
-	word.downcase.split(" ")
-
-	dictionary.reduce(Hash.new(0)) do |instances, word|
-		instances[word] += 1
+def substrings(string, dictionary)
+	#Convert string to lowercase array elements
+	array = string.downcase.split(" ")
+	
+	#Iterate through string array with reduce, transform array to new hash
+	array.reduce(Hash.new(0)) do |instances, word|
+		#Iterate through dictionary elements, if each string word matches an element word, add an instance
+		dictionary.each do |element|
+			if word.include?(element)
+				instances[element] += 1
+			end
+		end
+		#Store each iteration match in instances
 		instances
 	end
+
 end
 
-p substrings(word, dictionary) 
+puts substrings(string, dictionary) 
